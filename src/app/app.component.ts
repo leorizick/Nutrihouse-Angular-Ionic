48826f4a8c61cbd73f5c66af9156a368d238290d
Leonardo Rizick <leorizick@hotmail.com>
2022-06-13 09:11:57 -0300

    Botao flutuante do carrinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,7 @@ export class MyApp {
       {title: 'Menu', component: 'MenuPages'},
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      {title: 'Carrinho', component: 'CartPage'},
     
       {title: 'Logout', component: ''}
     ];
